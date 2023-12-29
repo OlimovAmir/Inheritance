4e823a37e55c514798f8e3503e1cd07992e3a455
olimov Amir <olimov.amir@mail.ru>
2023-12-29 11:29:45 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        
+
+        public void PrintName()
+        {
+            Console.WriteLine($"My name is\t {FirstName}");
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Person person = new Person { FirstName = "Ilhom", LastName="Olimov", Age = 39 };
+
+        person.PrintName();
+
+        Console.WriteLine(person);
+    }
+}
